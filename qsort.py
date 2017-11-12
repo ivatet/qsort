4b1f6023ec8ce948e9e18119f23350b82e4633d4
xpdu <teterevkov.ivan@gmail.com>
2017-11-12 18:41:33 +0000

    upd: haskell-like impl-on

--- qsort.py
+++ qsort.py
@@ -4,29 +4,14 @@ import copy
 import random
 
 
-def partial_impl(arr, low, high):
-    pi = arr[high]
-    i = low
-    for j in xrange(low, high):
-        if (arr[j] < pi):
-            arr[i], arr[j] = arr[j], arr[i]
-            i = i + 1
-
-    arr[i], arr[high] = arr[high], arr[i]
-    return i
-
-
-def qsort_impl(arr, low, high):
-    if (low < high):
-        pi = partial_impl(arr, low, high)
-        qsort_impl(arr, low, pi - 1)
-        qsort_impl(arr, pi + 1, high)
-
-
-def qsort(arr):
-    copied_arr = copy.copy(arr)
-    qsort_impl(copied_arr, 0, len(copied_arr) - 1)
-    return copied_arr
+def qsort(l):
+    if not l:
+        return []
+    else:
+        x, xs = l[0], l[1:]
+        lhs = [p for p in xs if p < x]
+        rhs = [p for p in xs if p > x]
+        return qsort(lhs) + [x] + qsort(rhs)
 
 
 def main():
